25c5dd7ea7c2e97435fb815beb1be9eb7100701d
John Hutchison <jhutchison@pivotal.io>
2019-08-13 14:01:33 -0400

    added commands to start server and locator in setUpLocalServer.sh

--- hello-world/local-server-config/setUpLocalServer.sh
+++ hello-world/local-server-config/setUpLocalServer.sh
@@ -1,4 +1,8 @@
 #start a locator
-#start a server 
+#start a server
+gfsh  -e  "start locator --name=locator" -e "start server --name=server"
+
+
+
 # that's all ?
 # what do I need to have gemfire set up correctly?  
