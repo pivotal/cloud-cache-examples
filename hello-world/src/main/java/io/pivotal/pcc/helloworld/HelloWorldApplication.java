f7b6aa9cf5a72885385ffb85d55d8446ace439bb
Ray Ingles <ringles@pivotal.io>
2019-08-16 10:14:35 -0400

    Add explanatory comments for PCC annotations

--- hello-world/src/main/java/io/pivotal/pcc/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/io/pivotal/pcc/helloworld/HelloWorldApplication.java
@@ -6,8 +6,8 @@ import org.springframework.data.gemfire.config.annotation.EnableCachingDefinedRe
 import org.springframework.data.gemfire.config.annotation.EnableClusterConfiguration;
 
 @SpringBootApplication
-@EnableCachingDefinedRegions
-@EnableClusterConfiguration(useHttp = true)
+@EnableCachingDefinedRegions // Creates PCC/Gemfire Regions via the @Cacheable annotation in HelloWorldService
+@EnableClusterConfiguration(useHttp = true) // Allows Spring to configure the PCC/Gemfire cluster; necessary for creating Regions
 public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
