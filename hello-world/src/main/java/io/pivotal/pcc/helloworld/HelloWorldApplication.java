6fa0dff2b4c554041ffad428afe290c35438465e
Karen Miller <kmiller@pivotal.io>
2019-08-20 16:19:44 -0700

    Reword two comments. Existing comments are unchanged, and should be removed if the changes are accepted. [#167943545]

--- hello-world/src/main/java/io/pivotal/pcc/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/io/pivotal/pcc/helloworld/HelloWorldApplication.java
@@ -7,7 +7,9 @@ import org.springframework.data.gemfire.config.annotation.EnableClusterConfigura
 
 @SpringBootApplication
 @EnableCachingDefinedRegions // Creates PCC/Gemfire Regions via the @Cacheable annotation in HelloWorldService
-@EnableClusterConfiguration(useHttp = true) // Allows Spring to configure the PCC/Gemfire cluster; necessary for creating Regions
+// Causes the creation of server-side PCC/GemFire regions via the @Cacheable annotation during the
+// initialization phase of the app's lifecycle
+@EnableClusterConfiguration(useHttp = true) // Allows Spring to configure the PCC/Gemfire cluster; necessary for creating regions
 public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
--- hello-world/src/main/java/io/pivotal/pcc/helloworld/HelloWorldService.java
+++ hello-world/src/main/java/io/pivotal/pcc/helloworld/HelloWorldService.java
@@ -13,7 +13,9 @@ public class HelloWorldService {
     // @Cacheable automatically uses argument of getHelloValue() as key, caches returned value
     // (Region with name "Hello" will be created via @EnableCachingDefinedRegions on Application)
     @Cacheable("Hello")
+    // @Cacheable uses the parameter of the method it annotates as the key and caches the return value.
     // This code will not be called if key/value pair found in cache
+    // This method will not be invoked if the key is in the cache
     public String getHelloValue(String key) {
         simulateSlowDataStore();
 
