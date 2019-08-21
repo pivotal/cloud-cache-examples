050f8ec456d8defa0d7de1f5296296a04b846846
Ray Ingles <ringles@pivotal.io>
2019-08-21 13:50:18 -0400

    Reconcile annotation comments

--- hello-world/src/main/java/io/pivotal/pcc/helloworld/HelloWorldService.java
+++ hello-world/src/main/java/io/pivotal/pcc/helloworld/HelloWorldService.java
@@ -10,11 +10,9 @@ import java.time.ZoneId;
 @Service
 public class HelloWorldService {
 
-    // @Cacheable automatically uses argument of getHelloValue() as key, caches returned value
+    // @Cacheable uses the parameter of the method it annotates as the key and caches the return value.
     // (Region with name "Hello" will be created via @EnableCachingDefinedRegions on Application)
     @Cacheable("Hello")
-    // @Cacheable uses the parameter of the method it annotates as the key and caches the return value.
-    // This code will not be called if key/value pair found in cache
     // This method will not be invoked if the key is in the cache
     public String getHelloValue(String key) {
         simulateSlowDataStore();
