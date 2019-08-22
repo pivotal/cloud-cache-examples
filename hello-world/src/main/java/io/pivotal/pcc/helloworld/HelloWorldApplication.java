c5c56f126a8720820a6aebffe03f61b93f42849e
Sarah <sabbey@pivotal.io>
2019-08-22 15:58:38 -0400

    Adds local and cloud config for SpringBoot 2.1.7

--- hello-world/src/main/java/io/pivotal/pcc/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/io/pivotal/pcc/helloworld/HelloWorldApplication.java
@@ -2,19 +2,32 @@ package io.pivotal.pcc.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.gemfire.config.annotation.EnableCachingDefinedRegions;
 import org.springframework.data.gemfire.config.annotation.EnableClusterConfiguration;
 
 @SpringBootApplication
-
 // Causes the creation of server-side PCC/GemFire regions via the @Cacheable annotation during the
-// initialization phase of the app's lifecycle
+// Initialization phase of the app's lifecycle
 @EnableCachingDefinedRegions
-
-// Allows Spring to configure the PCC/Gemfire cluster; necessary for creating regions
-@EnableClusterConfiguration(useHttp = true)
-
 public class HelloWorldApplication {
+
+	@Configuration
+	@Profile("!localCluster")
+	// Allows Spring to configure the PCC/Gemfire cluster; necessary for creating regions
+	@EnableClusterConfiguration(useHttp = true)
+	static class CloudConfiguration {
+
+	}
+
+	@Configuration
+	@Profile("localCluster")
+	@EnableClusterConfiguration(useHttp = true, requireHttps = false)
+	static class LocalConfiguration {
+
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
