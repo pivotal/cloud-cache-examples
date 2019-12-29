2a3483712dfa5997ec796c8c7de76d6110c4898f
concourse <concourse>
2019-12-29 01:50:15 +0000

    CI push: Adds LICENSE file and header to all files

--- hello-world/src/main/java/io/pivotal/cloudcache/helloworld/HelloWorldService.java
+++ hello-world/src/main/java/io/pivotal/cloudcache/helloworld/HelloWorldService.java
@@ -1,11 +1,15 @@
 /*
- * Cloud Cache Examples
- *
- * Copyright (c) 2019-Present Pivotal Software, Inc. All Rights Reserved.
- *
- * This product is licensed to you under the Apache License, Version 2.0 (the "License"). You may not use this product except in compliance with the License.
- *
- * This product may include a number of subcomponents with separate copyright notices and license terms. Your use of these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE file.
+Copyright (C) 2019-Present Pivotal Software, Inc. All rights reserved.
+
+This program and the accompanying materials are made available under the terms of the under the Apache License, Version
+2.0 (the "License”); you may not use this file except in compliance with the License. You may obtain a copy of the
+License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+language governing permissions and limitations under the License.
  */
 
 package io.pivotal.cloudcache.helloworld;
